0bd788689395fa40c6f86040651ab6bd5d63ed8e
Sameer Kongath <zoomixinfo@gmail.com>
2024-09-29 10:55:58 +0530

    Code Updated

--- project/settings.py
+++ project/settings.py
@@ -49,9 +49,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'project.urls'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 TEMPLATES = [
     {
@@ -118,9 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRES = [
-    os.path.join(BASE_DIR, 'static')
-]
+# STATICFILES_DIRES = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
+STATIC_ROOT = BASE_DIR / "static"
 
 MEDIA_ROOT = BASE_DIR/ 'uploads'
 MEDIA_URL = '/media/'
